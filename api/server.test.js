e0f7d72f5e382fa2403fa5c9571d85b5ba222088
Harley Eiss <ice.eiss@gmail.com>
2023-12-08 01:28:15 -0500

    tests decribed

--- api/server.test.js
+++ api/server.test.js
@@ -2,6 +2,7 @@
 const request = require('supertest')
 const db = require('../data/dbConfig')
 const server = require('./server.js')
+const bcrypt = require('bcryptjs')
 
 beforeAll(async () => {
   await db.migrate.rollback()
@@ -14,3 +15,54 @@ afterAll(async () => {
   await db.destroy()
 })
 
+describe('auth-router.js', () => {
+  describe('registers a new account', () => {
+    it.todo('[1] In order to register a new account the client must provide `username` and `password`', () => {
+
+    })
+    it.todo('[2] responds with the correct message on SUCCESSFUL registration', () => {
+
+    })
+    it.todo('[3] responds with the correct message on missing `username` and `password`', () => {
+
+    })
+    it.todo('[4] responds with the correct message on "username taken"', () => {
+
+    })
+  })
+  describe('log into an existing account', () => {
+    it.todo('[5] In order to log into an existing account the client must provide `username` and `password`', () => {
+
+    })
+    it.todo('[6] responds with the correct message on SUCCESSFUL login', () => {
+
+    })
+    it.todo('[7] responds with the correct message on missing `username` and `password`', () => {
+
+    })
+    it.todo('[8] responds with the correct message due to `username` not existing in the db', () => {
+
+    })
+    it.todo('[9] responds with the correct message due to `password` being incorrect', () => {
+
+    })
+  })
+})
+
+describe('restricted.js', () => {
+  it.todo('[10] On valid token in the Authorization header, call next', () => {
+
+  })
+  it.todo('[11] responds with the correct message on missing token', () => {
+
+  })
+  it.todo('[12] responds with the correct message on invalid or expired token', () => {
+
+  })
+
+})
+
+describe('restricted.js', () => {
+  it.todo('[13] only logged-in users should have access to /api/jokes', () => {
+  })
+})
